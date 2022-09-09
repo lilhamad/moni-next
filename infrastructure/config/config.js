7e92534b8f35fc2a8a9ed05dd6480ea368830811
ahmed adewale <lilhamad8@gmail.com>
2022-09-09 16:31:30 +0100

    added db url

--- infrastructure/config/config.js
+++ infrastructure/config/config.js
@@ -8,7 +8,14 @@ dotenv.config();
 const dialect = process.env.DIALECT || 'postgres';
 module.exports = {
   development: {
-    url: process.env.DB_URL || '',
+    url: process.env.DB_URL || 'postgres://leiwiner:z-53sXBXSdxgiGXeX3-7MhuXV8NxL1gc@heffalump.db.elephantsql.com/leiwiner',
+    dialect,
+    logging: e => {
+      console.log(e);
+    }
+  }, 
+  production: {
+    url: process.env.DB_URL || 'postgres://leiwiner:z-53sXBXSdxgiGXeX3-7MhuXV8NxL1gc@heffalump.db.elephantsql.com/leiwiner',
     dialect,
     logging: e => {
       console.log(e);
